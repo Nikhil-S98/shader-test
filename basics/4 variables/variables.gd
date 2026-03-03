6d57f3ea1427e0be4129f5176a24e3822e04f2fa
Nikhil-S98 <ns98.nikhil@gmail.com>
2026-03-02 21:03:09 -0500

    initial commit

---
+++ basics/4 variables/variables.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#var tween = create_tween()
+	#tween.set_loops()
+	#tween.tween_property($Icon.material, 'shader_parameter/CircleStep', 0.185, 1)
+	#tween.tween_property($Icon.material, "shader_parameter/CircleStep", 0.55, 1)
+	#tween.tween_property($Icon.material, 'shader_parameter/Movement', Vector2(128.0, 0), 1)
+	#tween.tween_property($Icon.material, "shader_parameter/Movement", Vector2.ZERO, 1)
+	pass
